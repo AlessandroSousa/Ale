905f1d6a0ea77d8fa9cbddab280d7420b9266592
AlessandroSousa <medeiros.o.u@globomail.com>
2014-11-01 12:40:16 -0300

    Prog
    teste

---
+++ scripts/mycode.js
@@ -0,0 +1,27 @@
+﻿///// <reference path="mycode.js" />
+var app = angular.module('HackingCodeApp', []);
+
+
+app.controller('MainController', function ($rootScope, $http, $scope) {
+        
+    $scope.nome = "Ale";
+
+    $scope.registro = function () {
+        alert('Ale');
+        $http.post('http://localhost:9090/Empresa').success(RegistroCallback);
+    }
+
+    $scope.Clear = function () {
+        
+        $scope.Participante.fone = "";
+        $scope.Participante.email = "";
+        $scope.Participante.area = "";
+    }
+
+    function RegistroCallback(data, status) {
+        $scope.objeto = data;
+        alert($scope.objeto);
+    }
+
+    
+});
